1af736e642c499dca4d68f9e5c20acf05d44d258
Taras Romaniuk <tarasrom25@gmail.com>
2024-06-27 18:32:24 +0300

    allowed to create invoice

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,20 +1,24 @@
-import { fetchInvoicesPages } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import Pagination from '@/app/ui/invoices/pagination';
-import Table from '@/app/ui/invoices/table';
-import Search from '@/app/ui/search';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+import { fetchInvoicesPages } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import Table from "@/app/ui/invoices/table";
+import Search from "@/app/ui/search";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-export default async function Page({searchParams}: {searchParams: {
-    query?: string,
-    page?: string
-}}) {
-    const query = searchParams.query || ""
-    const currentPage = Number(searchParams.page) || 1
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams.query || "";
+  const currentPage = Number(searchParams.page) || 1;
 
-    const totalPages = await fetchInvoicesPages(query)
+  const totalPages = await fetchInvoicesPages(query);  
 
   return (
     <div className="w-full">
@@ -25,7 +29,7 @@ export default async function Page({searchParams}: {searchParams: {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,27 +1,26 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const { replace } = useRouter()
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  const handleSearch = useDebouncedCallback((term: string)=>{
-    const params = new URLSearchParams(searchParams)
-    params.set('page', '1');
-    
-    if(term) {
-      params.set("query", term)
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+
+    if (term) {
+      params.set("query", term);
     } else {
-      params.delete("query")
+      params.delete("query");
     }
 
-    replace(`${pathname}?${params.toString()}`)
-    }, 300)
-
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
